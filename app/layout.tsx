418fc8c95be44993346105d0c8b08d033f494ca9
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2025-03-31 19:48:19 +0200

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AidXBait',
+  description: 'AidXBait is a platform that connects patients with healthcare providers. We offer a wide range of services including physical therapy, orthopedics, general medicine, home visits, and online consultations.',
+  generator: 'aidxbait',
 }
 
 export default function RootLayout({
